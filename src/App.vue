a1a10421590db9867512a6d2d0095dc1e0913adf
Sami-Shafi <freelancersami123@gmail.com>
2022-07-27 01:14:51 +0600

    Vue Router Manual Routing

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,21 @@
 <script>
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
-import Tasks from "./components/Tasks.vue";
-import AddTask from "./components/AddTask.vue";
 
 export default {
 	name: "App",
-	components: { Header, Tasks, AddTask, Footer },
+	components: { Header, Footer },
 
 	data() {
 		return {
-			tasks: [],
 			showForm: false,
 		};
 	},
 
 	methods: {
-		async deleteTask(id) {
-			if (confirm("Are you sure?")) {
-				const res = await fetch(`/api/tasks/${id}`, {
-					method: "DELETE",
-				});
-
-				res.status === 200
-					? (this.tasks = this.tasks.filter((task) => task.id !== id))
-					: alert("Server Error!");
-			}
-		},
-
-		async toggleReminder(id) {
-			const selectedTask = await this.fetchSingleWork(id);
-			const updTask = {
-				...selectedTask,
-				reminder: !selectedTask.reminder,
-			};
-
-			const res = await fetch(`api/tasks/${id}`, {
-				method: "PUT",
-				headers: {
-					"Content-type": "application/json",
-				},
-				body: JSON.stringify(updTask),
-			});
-
-			const data = await res.json();
-
-			this.tasks = this.tasks.map((task) =>
-				task.id === id ? { ...task, reminder: data.reminder } : task
-			);
-		},
-		async addTask(task) {
-			const res = await fetch("/api/tasks", {
-				method: "POST",
-				headers: {
-					"Content-type": "application/json",
-				},
-				body: JSON.stringify(task),
-			});
-
-			const data = await res.json();
-
-			this.tasks = [...this.tasks, data];
-		},
-
 		toggleForm() {
 			this.showForm = !this.showForm;
 		},
-
-		async fetchSingleWork(id) {
-			const res = await fetch(`/api/tasks/${id}`);
-			const data = await res.json();
-			return data;
-		},
-
-		async fetchWorks() {
-			const res = await fetch("/api/tasks");
-			const data = await res.json();
-			return data;
-		},
-	},
-
-	async created() {
-		this.tasks = await this.fetchWorks();
 	},
 };
 </script>
@@ -93,19 +27,20 @@ export default {
 			:formShown="showForm"
 			@toggle-Form="toggleForm"
 		/>
-		<transition name="fade">
+		<!-- <transition name="fade">
 			<div v-if="showForm">
 				<AddTask @add-task="addTask" />
 			</div>
-		</transition>
+		</transition> -->
 
-        <router-view></router-view>
-
-		<Tasks
+		<!-- <Tasks
 			:tasks="tasks"
 			@delete-task="deleteTask"
 			@toggle-reminder="toggleReminder"
-		/>
+		/> -->
+
+		<router-view :showForm="showForm"></router-view>
+
 		<Footer />
 	</div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,97 @@
 <script>
-    export default {
-        name: "Home",
-    }
+import AddTask from "../components/AddTask.vue";
+import Tasks from "../components/Tasks.vue";
+
+export default {
+	name: "Home",
+	components: { Tasks, AddTask },
+	props: { showForm: Boolean },
+	data() {
+		return {
+			tasks: [],
+		};
+	},
+
+	methods: {
+		async deleteTask(id) {
+			if (confirm("Are you sure?")) {
+				const res = await fetch(`/api/tasks/${id}`, {
+					method: "DELETE",
+				});
+
+				res.status === 200
+					? (this.tasks = this.tasks.filter((task) => task.id !== id))
+					: alert("Server Error!");
+			}
+		},
+
+		async toggleReminder(id) {
+			const selectedTask = await this.fetchSingleWork(id);
+			const updTask = {
+				...selectedTask,
+				reminder: !selectedTask.reminder,
+			};
+
+			const res = await fetch(`api/tasks/${id}`, {
+				method: "PUT",
+				headers: {
+					"Content-type": "application/json",
+				},
+				body: JSON.stringify(updTask),
+			});
+
+			const data = await res.json();
+
+			this.tasks = this.tasks.map((task) =>
+				task.id === id ? { ...task, reminder: data.reminder } : task
+			);
+		},
+		async addTask(task) {
+			const res = await fetch("/api/tasks", {
+				method: "POST",
+				headers: {
+					"Content-type": "application/json",
+				},
+				body: JSON.stringify(task),
+			});
+
+			const data = await res.json();
+
+			this.tasks = [...this.tasks, data];
+		},
+
+		async fetchSingleWork(id) {
+			const res = await fetch(`/api/tasks/${id}`);
+			const data = await res.json();
+			return data;
+		},
+
+		async fetchWorks() {
+			const res = await fetch("/api/tasks");
+			const data = await res.json();
+			return data;
+		},
+	},
+
+	async created() {
+		this.tasks = await this.fetchWorks();
+	},
+};
 </script>
 
 <template>
-    <h2>Home</h2>
+	<!-- <h2>Home</h2> -->
+	<transition name="fade">
+		<div v-if="showForm">
+			<AddTask @add-task="addTask" />
+		</div>
+	</transition>
+
+	<Tasks
+		:tasks="tasks"
+		@delete-task="deleteTask"
+		@toggle-reminder="toggleReminder"
+	/>
 </template>
 
 <style></style>
